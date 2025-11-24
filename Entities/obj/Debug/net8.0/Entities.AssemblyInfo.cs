c6f294e32a300b0ddbe2b21eb81b0720cefe7481
Ceyhun Yusif <jeyhun312@gmail.com>
2025-11-24 18:23:06 +0400

    Cookie Config Elave Edildi

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be26f30d60c89bc4543afb487c5ef9ca50d462a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050706b3d67c698878187e29e5acdffc919d755a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
